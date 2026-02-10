b6d0669e2f04c6fa82d9665dd6bc46fa50220b7c
vitaly <vitaly99878@gmail.com>
2026-02-10 16:26:33 +0300

    ABC-001 chore(config): ESLint до “проектного”
    react-hooks — чтобы ловить нарушения хуков.
    router/query — best practices для TanStack.
    eslint-config-prettier — чтобы ESLint не спорил с Prettier
    
    добавил ignores, чтобы не линтить конфиги форматтера/линтера и служебные скрипты
    imports are fixed

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,40 @@
-//  @ts-check
+// @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginRouter from '@tanstack/eslint-plugin-router'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
-export default [...tanstackConfig]
+export default [
+  {
+    ignores: [
+      'eslint.config.js',
+      'prettier.config.*',
+      'commitlint.config.*',
+      'scripts/**',
+      '.husky/**',
+      'dist/**',
+    ],
+  },
+
+  ...tanstackConfig,
+
+  // React Hooks
+  {
+    plugins: { 'react-hooks': reactHooks },
+    rules: reactHooks.configs.recommended.rules,
+  },
+
+  // TanStack Router
+  ...pluginRouter.configs['flat/recommended'],
+
+  // TanStack Query
+  ...pluginQuery.configs['flat/recommended'],
+
+  // Игнорим генерируемое дерево роутов
+  { ignores: ['**/routeTree.gen.ts'] },
+
+  // Prettier — последним
+  eslintConfigPrettier,
+]
