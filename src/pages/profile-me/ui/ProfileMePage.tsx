b08fd7fbe4902605e1d7fa150ed4a2b0340f6e9d
vitaly <vitaly99878@gmail.com>
2026-02-20 13:44:30 +0300

    refactor: import is fixed

--- src/pages/profile-me/ui/ProfileMePage.tsx
+++ src/pages/profile-me/ui/ProfileMePage.tsx
@@ -1,9 +1,9 @@
 import { api } from '@/shared/api'
 import { apiQueryKey, useApiQuery } from '@/shared/api/reactQuery'
-import { PageQueryGuard } from '@/shared/ui/PageQueryGuard/PageQueryGuard'
 import { Button, LinkAsButton } from '@/shared/ui/button/index'
 import { Input } from '@/shared/ui/input/Input'
 import { Field } from '@/shared/ui/field/Field'
+import { PageQueryGuard } from '@/shared/ui/page-query-guard/PageQueryGuard'
 
 export function ProfileMePage() {
   const queryKey = apiQueryKey(api.auth.me)
